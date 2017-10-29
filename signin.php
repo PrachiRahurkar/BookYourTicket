2710d3e1ee8e97b40522a96b1a241a3d526dec36
Prachi Rahurkar <rahurkarprachi@gmail.com>
2017-10-29 22:15:47 +0530

    Add files via upload

---
+++ signin.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Username:: <?php echo $_POST["username"]; ?><br>
+Password:: <?php echo $_POST["password"]; ?><br/>
+<a href="index.html">Return to home page </a>
+
+</body>
+</html>
